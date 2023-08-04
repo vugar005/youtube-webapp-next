30f3e9acfa67bbd66fd042b42b4ed17d571d5d6e
vugar <main@vugar.app>
2023-08-04 18:23:24 +0200

    fix global styles override in NextJs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,20 @@ import { Provider } from 'react-redux';
 import store from "../store/index";
 import DarkThemeProvider from '@/providers/dark-theme-provider/dark-theme-provider';
 import MiniPlayerWrapper from '@/components/mini-player-wrapper/mini-player-wrapper';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Provider store={store}>
-      <DarkThemeProvider>
-        <Layout>
-          <Component {...pageProps} />
-          <MiniPlayerWrapper/>
-        </Layout>
-      </DarkThemeProvider>
+      <StyledEngineProvider injectFirst>
+        <DarkThemeProvider>
+          <Layout>
+            <Component {...pageProps} />
+            <MiniPlayerWrapper />
+          </Layout>
+        </DarkThemeProvider>
+      </StyledEngineProvider>
     </Provider>
   );
 }
